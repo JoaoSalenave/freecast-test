f44889c8f734b10f8231602b95bb2992ff29b981
JoaoSalenave <joaog.salenave@gmail.com>
2025-06-04 07:18:30 -0300

    chore: setting up models and admin

--- media/catalog/admin.py
+++ media/catalog/admin.py
@@ -1,3 +1,40 @@
 from django.contrib import admin
+from .models import Show, Season, Episode, Movie, Source
 
-# Register your models here.
+@admin.register(Show)
+class ShowAdmin(admin.ModelAdmin):
+    list_display = ('title', 'release_date', 'imdb_rating', 'kinopoisk_rating')
+    search_fields = ('title',)
+    ordering = ('title',)
+
+@admin.register(Season)
+class SeasonAdmin(admin.ModelAdmin):
+    list_display = ('show', 'number')
+    list_filter = ('show',)
+    ordering = ('show__title', 'number')
+
+@admin.register(Episode)
+class EpisodeAdmin(admin.ModelAdmin):
+    list_display = ('title', 'season', 'number', 'release_date')
+    list_filter = ('season__show',)
+    ordering = ('season__show__title', 'season__number', 'number')
+
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ('title', 'release_date', 'imdb_rating', 'kinopoisk_rating')
+    search_fields = ('title',)
+    ordering = ('title',)
+
+@admin.register(Source)
+class SourceAdmin(admin.ModelAdmin):
+    list_display = ('url', 'source_type', 'linked_movie', 'linked_episode')
+    list_filter = ('source_type',)
+    search_fields = ('url',)
+
+    def linked_movie(self, obj):
+        return obj.movie
+    linked_movie.short_description = 'Movie'
+
+    def linked_episode(self, obj):
+        return obj.episode
+    linked_episode.short_description = 'Episode'
--- media/catalog/models.py
+++ media/catalog/models.py
@@ -1,3 +1,62 @@
 from django.db import models
 
-# Create your models here.
+class Show(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    image = models.URLField()
+    release_date = models.DateField()
+    imdb_rating = models.FloatField()
+    kinopoisk_rating = models.FloatField()
+
+    def __str__(self):
+        return self.title
+
+class Season(models.Model):
+    show = models.ForeignKey(Show, related_name='seasons', on_delete=models.CASCADE)
+    number = models.PositiveIntegerField()
+    description = models.TextField(blank=True)
+
+    class Meta:
+        ordering = ['number']
+
+    def __str__(self):
+        return f'{self.show.title} - Season {self.number}'
+
+class Episode(models.Model):
+    season = models.ForeignKey(Season, related_name='episodes', on_delete=models.CASCADE)
+    number = models.PositiveIntegerField()
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    release_date = models.DateField()
+
+    class Meta:
+        ordering = ['number']
+
+    def __str__(self):
+        return f'{self.season.show.title} S{self.season.number}E{self.number} - {self.title}'
+
+class Movie(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    image = models.URLField()
+    release_date = models.DateField()
+    imdb_rating = models.FloatField()
+    kinopoisk_rating = models.FloatField()
+
+    def __str__(self):
+        return self.title
+
+SOURCE_TYPE_CHOICES = [
+    ('playlist', 'Playlist'),
+    ('direct', 'Direct'),
+]
+
+class Source(models.Model):
+    movie = models.ForeignKey(Movie, related_name='sources', null=True, blank=True, on_delete=models.CASCADE)
+    episode = models.ForeignKey(Episode, related_name='sources', null=True, blank=True, on_delete=models.CASCADE)
+    url = models.URLField()
+    source_type = models.CharField(max_length=20, choices=SOURCE_TYPE_CHOICES)
+
+    def __str__(self):
+        target = self.movie or self.episode
+        return f'{target} - {self.get_source_type_display()}'
