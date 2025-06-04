aa5ade452a549e0c9c9ff90654ecebf35559db34
JoaoSalenave <joaog.salenave@gmail.com>
2025-06-04 17:03:04 -0300

    fix: ordering in Django admin

--- media/catalog/admin.py
+++ media/catalog/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import Show, Season, Episode, Movie, Source
+from django.db.models.functions import Coalesce
+from django.db.models import Case, IntegerField, Value, When
 
 @admin.register(Show)
 class ShowAdmin(admin.ModelAdmin):
@@ -27,14 +29,37 @@ class MovieAdmin(admin.ModelAdmin):
 
 @admin.register(Source)
 class SourceAdmin(admin.ModelAdmin):
-    list_display = ('url', 'source_type', 'linked_movie', 'linked_episode')
-    list_filter = ('source_type',)
-    search_fields = ('url',)
+    list_display  = ("url", "source_type", "linked_movie", "linked_episode")
+    list_filter   = ("source_type",)
+    search_fields = ("url",)
 
     def linked_movie(self, obj):
         return obj.movie
-    linked_movie.short_description = 'Movie'
+    linked_movie.short_description = "Movie"
 
     def linked_episode(self, obj):
         return obj.episode
-    linked_episode.short_description = 'Episode'
+    linked_episode.short_description = "Episode"
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+
+        qs = qs.annotate(
+            is_movie=Case(
+                When(movie__isnull=False, then=Value(0)),
+                default=Value(1),
+                output_field=IntegerField(),
+            ),
+            sort_title=Coalesce(
+                "movie__title",                  
+                "episode__season__show__title"   
+            )
+        ).order_by(
+            "is_movie",
+            "sort_title",
+            "episode__season__number",
+            "episode__number",
+            "id",
+        )
+
+        return qs
